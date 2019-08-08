04abdc7971728e26b355f19113137e1a3ef6b119
Santa Claus <joselynetusingwir@gmail.com>
2019-08-08 17:47:07 +0200

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    // $("#frm-0").submit(function(event){ 
+    //     event.preventDefault();
+   $(".interior-design").click(function(){
+    $("#frm-0").show();
+   
+   })
+     $("#next").click(function(){
+        alert('Thank you for ordering we will contact you.');
+    })
+})
